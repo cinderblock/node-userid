c6ee7906fd32eeddc29a2d70a507bf652dc3690c
Cameron Tacklind <cameron@tacklind.com>
2022-01-03 13:28:48 -0800

    Test environment uses es2022

--- test/test.ts
+++ test/test.ts
@@ -7,50 +7,50 @@ import * as types from '../src/userid';
 
 const importPath = process.env.TEST_IMPORT_OVERRIDE || '../src/userid.cjs';
 
-const userid = (import(importPath) as Promise<typeof types>).catch(e => {
+const userid = (await import(importPath).catch(e => {
   console.error(e);
   throw e;
-});
+})) as typeof types;
 
 // Simply test against current user's info
 
-const shellUsername = execToString('id -un');
-const shellGroupName = execToString('id -gn');
+const shellUsername = await execToString('id -un');
+const shellGroupName = await execToString('id -gn');
 
-const shellUid = execToString('id -u').then(Number);
-const shellGid = execToString('id -g').then(Number);
+const shellUid = await execToString('id -u').then(Number);
+const shellGid = await execToString('id -g').then(Number);
 
 // Get a nice list
-const shellGids = execToString('id -G').then(gids => gids.split(' ').map(Number).sort());
+const shellGids = await execToString('id -G').then(gids => gids.split(' ').map(Number).sort());
 
 // As per https://github.com/cinderblock/node-userid/issues/3 we need to test long usernames.
 // But we don't want to fail if a username is missing.
 const longUsername = process.env.TEST_LONG_USERNAME;
-const longUsernameUid = longUsername ? execToString(`id -u ${longUsername}`).then(Number) : -1;
+const longUsernameUid = longUsername ? await execToString(`id -u ${longUsername}`).then(Number) : -1;
 
 // Try long group names too
 const longGroupName = process.env.TEST_LONG_GROUPNAME;
-const longGroupnameGid = longGroupname ? execToString(`id -g ${longGroupname}`).then(Number) : -1;
+const longGroupNameGid = longGroupName ? await execToString(`id -g ${longGroupName}`).then(Number) : -1;
 
-async function testIdsMethod() {
-  it(`should load a user's uid [${await shellUid}] and gid [${await shellGid}] by username [${await shellUsername}] as an object`, async () => {
-    const libIds = (await userid).ids(await shellUsername);
+function testIdsMethod() {
+  it(`should load a user's uid [${shellUid}] and gid [${shellGid}] by username [${shellUsername}] as an object`, () => {
+    const libIds = userid.ids(shellUsername);
 
-    libIds.uid.should.equal(await shellUid);
-    libIds.gid.should.equal(await shellGid);
+    libIds.uid.should.equal(shellUid);
+    libIds.gid.should.equal(shellGid);
   });
 
   if (longUsername) {
-    it(`should work with a long username [${longUsername}] that exists`, async () => {
-      const libLongIds = (await userid).ids(longUsername);
+    it(`should work with a long username [${longUsername}] that exists`, () => {
+      const libLongIds = userid.ids(longUsername);
 
-      libLongIds.uid.should.equal(await longUsernameUid);
+      libLongIds.uid.should.equal(longUsernameUid);
 
       // TODO: Test `libLongIds.gid.should.equal(shellGid)` properly
     });
   }
 
-  itShouldHandleErrorsConsistently((await userid).ids, 'string', 'username', 'username not found');
+  itShouldHandleErrorsConsistently(userid.ids, 'string', 'username', 'username not found');
 }
 
 describe('userid', () => {
@@ -62,74 +62,71 @@ describe('userid', () => {
     describe('method userid.uid (should match method `userid.ids`)', testIdsMethod);
   } else {
     // Otherwise it has its own signature
-    describe('method userid.uid', async () => {
-      it(`should load user's uid [${await shellUid}] by username [${await shellUsername}]`, async () => {
-        (await userid).uid(await shellUsername).should.equal(await shellUid);
+    describe('method userid.uid', () => {
+      it(`should load user's uid [${shellUid}] by username [${shellUsername}]`, () => {
+        userid.uid(shellUsername).should.equal(shellUid);
       });
 
       if (longUsername) {
-        it(`should work with a long username [${longUsername}] that exists`, async () => {
-          (await userid).uid(longUsername).should.equal(await longUsernameUid);
+        it(`should work with a long username [${longUsername}] that exists`, () => {
+          userid.uid(longUsername).should.equal(longUsernameUid);
         });
       }
 
-      itShouldHandleErrorsConsistently((await userid).uid, 'string', 'username', 'username not found');
+      itShouldHandleErrorsConsistently(userid.uid, 'string', 'username', 'username not found');
     });
   }
 
-  describe('method userid.username', async () => {
-    it(`should load a username [${await shellUsername}] by uid [${await shellUid}]`, async () => {
-      (await userid).username(await shellUid).should.equal(await shellUsername);
+  describe('method userid.username', () => {
+    it(`should load a username [${shellUsername}] by uid [${shellUid}]`, () => {
+      userid.username(shellUid).should.equal(shellUsername);
     });
 
     if (longUsername) {
-      it(`should load a long username [${longUsername}] by uid [${await longUsernameUid}]`, async () => {
-        (await userid).username(await longUsernameUid).should.equal(longUsername);
+      it(`should load a long username [${longUsername}] by uid [${longUsernameUid}]`, () => {
+        userid.username(longUsernameUid).should.equal(longUsername);
       });
     }
 
-    itShouldHandleErrorsConsistently((await userid).username, 'number', 'uid', 'uid not found');
+    itShouldHandleErrorsConsistently(userid.username, 'number', 'uid', 'uid not found');
   });
 
-  describe('method userid.gid', async () => {
-    it(`should load group's gid [${await shellGid}] by group name [${await shellGroupName}]`, async () => {
-      (await userid).gid(await shellGroupName).should.equal(await shellGid);
+  describe('method userid.gid', () => {
+    it(`should load group's gid [${shellGid}] by group name [${shellGroupName}]`, () => {
+      userid.gid(shellGroupName).should.equal(shellGid);
     });
 
-    if (longGroupname) {
-      it(`should work with a long groupname [${longGroupname}] that exists`, async () => {
-        (await userid).gid(longGroupname).should.equal(await longGroupnameGid);
+    if (longGroupName) {
+      it(`should work with a long groupname [${longGroupName}] that exists`, () => {
+        userid.gid(longGroupName).should.equal(longGroupNameGid);
       });
     }
 
-    itShouldHandleErrorsConsistently((await userid).gid, 'string', 'groupname', 'groupname not found');
+    itShouldHandleErrorsConsistently(userid.gid, 'string', 'groupname', 'groupname not found');
   });
 
-  describe('method userid.groupname', async () => {
-    it(`should load a group's name [${await shellGroupName}] by gid [${await shellGid}]`, async () => {
-      (await userid).groupname(await shellGid).should.equal(await shellGroupName);
+  describe('method userid.groupname', () => {
+    it(`should load a group's name [${shellGroupName}] by gid [${shellGid}]`, () => {
+      userid.groupname(shellGid).should.equal(shellGroupName);
     });
 
-    if (longGroupname) {
-      it(`should work with a long groupname [${longGroupname}] that exists`, async () => {
-        (await userid).groupname(await longGroupnameGid).should.equal(longGroupname);
+    if (longGroupName) {
+      it(`should work with a long groupname [${longGroupName}] that exists`, () => {
+        userid.groupname(longGroupNameGid).should.equal(longGroupName);
       });
     }
 
-    itShouldHandleErrorsConsistently((await userid).groupname, 'number', 'gid', 'gid not found');
+    itShouldHandleErrorsConsistently(userid.groupname, 'number', 'gid', 'gid not found');
   });
 
-  describe('method userid.gids', async () => {
-    it(`should load a list of gids [${await shellGids}] by username [${await shellUsername}]`, async () => {
-      (await userid)
-        .gids(await shellUsername)
-        .sort()
-        .should.deepEqual(await shellGids);
+  describe('method userid.gids', () => {
+    it(`should load a list of gids [${shellGids}] by username [${shellUsername}]`, () => {
+      userid.gids(shellUsername).sort().should.deepEqual(shellGids);
     });
 
     // TODO: test for a long username
     // TODO: test for a long groupname in returned list
 
-    itShouldHandleErrorsConsistently((await userid).gids, 'string', 'user', 'getpwnam');
+    itShouldHandleErrorsConsistently(userid.gids, 'string', 'user', 'getpwnam');
   });
 });
