c122ac3550eb9706bce77af8b9a80fca81f2e254
Cameron Tacklind <cameron@tacklind.com>
2022-01-03 14:08:41 -0800

    Fix implementation

--- test/lib/execToString.ts
+++ test/lib/execToString.ts
@@ -1,10 +1,14 @@
 import { exec } from 'child_process';
-import { promisify } from 'util';
 
 /**
  * Run a command synchronously and get the output as a string with newlines stripped
  * @param command The command to run
  */
-export default async function execToString(command: string): Promise<string> {
-  return (await promisify(exec)(command)).toString().replace('\n', '');
+export default function execToString(command: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => {
+      if (err) return reject(stderr);
+      resolve(stdout.replace('\n', ''));
+    });
+  });
 }
