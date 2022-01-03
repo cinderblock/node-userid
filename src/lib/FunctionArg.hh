201986fd55486bdf4393e1ba54cd1f6b416aeadd
Cameron Tacklind <cameron@tacklind.com>
2022-01-02 21:42:50 -0800

    Declare specialization before implicit instantiation

--- src/lib/FunctionArg.hh
+++ src/lib/FunctionArg.hh
@@ -4,4 +4,6 @@
 
 namespace userid {
 template <typename T> auto getArg(const Napi::CallbackInfo &info, unsigned index = 0) -> T;
+template <> auto getArg(const Napi::CallbackInfo &info, unsigned index) -> int32_t;
+template <> auto getArg(const Napi::CallbackInfo &info, unsigned index) -> std::string;
 } // namespace userid
