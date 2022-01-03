0befa579e44d5ca4a3ef2a0aae2bcde7f51c8513
Cameron Tacklind <cameron@tacklind.com>
2022-01-03 14:04:57 -0800

    Fix bug in function implementation

--- src/lib/FunctionArg.cc
+++ src/lib/FunctionArg.cc
@@ -6,7 +6,7 @@ using Napi::String;
 using Napi::TypeError;
 
 template <> auto userid::getArg(const CallbackInfo &info, unsigned index) -> int32_t {
-  if (info.Length() < index) {
+  if (info.Length() < index + 1) {
     throw TypeError::New(info.Env(), "Wrong number of arguments");
   }
 
@@ -18,7 +18,7 @@ template <> auto userid::getArg(const CallbackInfo &info, unsigned index) -> int
 }
 
 template <> auto userid::getArg(const CallbackInfo &info, unsigned index) -> std::string {
-  if (info.Length() < index) {
+  if (info.Length() < index + 1) {
     throw TypeError::New(info.Env(), "Wrong number of arguments");
   }
 
