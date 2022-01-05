0d4849c33bbdc6ea974fe68401c8dbddb0dc7359
Cameron Tacklind <cameron@tacklind.com>
2022-01-05 13:05:22 -0800

    Comments

--- src/lib/FunctionArg.cc
+++ src/lib/FunctionArg.cc
@@ -6,10 +6,12 @@ using Napi::String;
 using Napi::TypeError;
 
 template <> auto userid::getArg(const CallbackInfo &info, unsigned index) -> int32_t {
+  // CallbackInfo::operator[] doesn't throw when out of bounds
   if (info.Length() <= index) {
     throw TypeError::New(info.Env(), "Wrong number of arguments");
   }
 
+  // Improve error message
   if (!info[index].IsNumber()) {
     throw TypeError::New(info.Env(), "Argument must be a number");
   }
@@ -18,10 +20,12 @@ template <> auto userid::getArg(const CallbackInfo &info, unsigned index) -> int
 }
 
 template <> auto userid::getArg(const CallbackInfo &info, unsigned index) -> std::string {
+  // CallbackInfo::operator[] doesn't throw when out of bounds
   if (info.Length() <= index) {
     throw TypeError::New(info.Env(), "Wrong number of arguments");
   }
 
+  // Improve error message
   if (!info[index].IsString()) {
     throw TypeError::New(info.Env(), "Argument must be a string");
   }
