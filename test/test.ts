ffe48dc6a47062e070a455e221b6af72bb896613
Cameron Tacklind <cameron@tacklind.com>
2022-01-05 13:21:33 -0800

    Long username gid test

--- test/test.ts
+++ test/test.ts
@@ -17,22 +17,20 @@ const userid = (
 // Simply test against current user's info
 
 const shellUsername = await execToString('id -un');
-const shellGroupName = await execToString('id -gn');
-
+const shellGroupname = await execToString('id -gn');
 const shellUid = await execToString('id -u').then(Number);
 const shellGid = await execToString('id -g').then(Number);
-
-// Get a nice list
 const shellGids = await execToString('id -G').then(gids => gids.split(' ').map(Number));
 
 // As per https://github.com/cinderblock/node-userid/issues/3 we need to test long usernames.
 // But we don't want to fail if a username is missing.
 const longUsername = process.env.TEST_LONG_USERNAME;
 const longUsernameUid = longUsername ? await execToString(`id -u ${longUsername}`).then(Number) : -1;
+const longUsernameGid = longUsername ? await execToString(`id -g ${longUsername}`).then(Number) : -1;
 
 // Try long group names too
-const longGroupName = process.env.TEST_LONG_GROUPNAME;
-const longGroupNameGid = longGroupName ? await execToString(`id -g ${longGroupName}`).then(Number) : -1;
+const longGroupname = process.env.TEST_LONG_GROUPNAME;
+const longGroupnameGid = longGroupname ? await execToString(`id -g ${longGroupname}`).then(Number) : -1;
 
 function testIdsMethod() {
   it(`should load a user's uid [${shellUid}] and gid [${shellGid}] by username [${shellUsername}] as an object`, () => {
@@ -47,8 +45,7 @@ function testIdsMethod() {
       const libLongIds = userid.ids(longUsername);
 
       libLongIds.uid.should.equal(longUsernameUid);
-
-      // TODO: Test `libLongIds.gid.should.equal(shellGid)` properly
+      libLongIds.gid.should.equal(longUsernameGid);
     });
   }
 
@@ -94,13 +91,13 @@ describe('userid', () => {
   });
 
   describe('method userid.gid', () => {
-    it(`should load group's gid [${shellGid}] by group name [${shellGroupName}]`, () => {
-      userid.gid(shellGroupName).should.equal(shellGid);
+    it(`should load group's gid [${shellGid}] by group name [${shellGroupname}]`, () => {
+      userid.gid(shellGroupname).should.equal(shellGid);
     });
 
-    if (longGroupName) {
-      it(`should work with a long groupname [${longGroupName}] that exists`, () => {
-        userid.gid(longGroupName).should.equal(longGroupNameGid);
+    if (longGroupname) {
+      it(`should work with a long groupname [${longGroupname}] that exists`, () => {
+        userid.gid(longGroupname).should.equal(longGroupnameGid);
       });
     }
 
@@ -108,13 +105,13 @@ describe('userid', () => {
   });
 
   describe('method userid.groupname', () => {
-    it(`should load a group's name [${shellGroupName}] by gid [${shellGid}]`, () => {
-      userid.groupname(shellGid).should.equal(shellGroupName);
+    it(`should load a group's name [${shellGroupname}] by gid [${shellGid}]`, () => {
+      userid.groupname(shellGid).should.equal(shellGroupname);
     });
 
-    if (longGroupName) {
-      it(`should work with a long groupname [${longGroupName}] that exists`, () => {
-        userid.groupname(longGroupNameGid).should.equal(longGroupName);
+    if (longGroupname) {
+      it(`should work with a long groupname [${longGroupname}] that exists`, () => {
+        userid.groupname(longGroupnameGid).should.equal(longGroupname);
       });
     }
 
